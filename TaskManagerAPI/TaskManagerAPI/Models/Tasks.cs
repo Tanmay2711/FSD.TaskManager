2a4979ba7550493d01818f599d62bf32379a152d
Tanmay Vartak <tanmay.vartak@ey.com>
2019-06-07 11:59:41 +0530

    Added Tasks model

---
+++ TaskManagerAPI/TaskManagerAPI/Models/Tasks.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TaskManagerAPI.Models
+{
+    public class Tasks
+    {
+        public int TasksID { get; set; }
+        public int ParentID { get; set; }
+        public string Name { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+        public int Priority { get; set; }
+    }
+}
