879d1fe467a3fd2b4e9726ed4387ce01a83d7292
Tanmay Vartak <tanmay.vartak@ey.com>
2019-06-07 12:06:52 +0530

    Added TaskController and Context class

--- TaskManagerAPI/TaskManagerAPI/Startup.cs
+++ TaskManagerAPI/TaskManagerAPI/Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.EntityFrameworkCore;
+using TaskManagerAPI.Models;
 
 namespace TaskManagerAPI
 {
@@ -26,6 +28,9 @@ namespace TaskManagerAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddDbContext<TaskManagerContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("TaskManagerContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
