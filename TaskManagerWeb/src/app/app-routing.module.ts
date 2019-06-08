4afacba1a931fb7af59c6409b814d35c7a32db90
Tanmay Vartak <Tanmay.Vartak@ey.com>
2019-06-08 15:17:17 +0530

    Added basic routing features

--- TaskManagerWeb/src/app/app-routing.module.ts
+++ TaskManagerWeb/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ViewTaskComponent } from './view-task/view-task.component';
+import { AddTaskComponent } from './add-task/add-task.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'viewtask', component: ViewTaskComponent },
+  { path: 'addtask', component: AddTaskComponent },
+  { path: '',   redirectTo: '/viewtask', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- TaskManagerWeb/src/app/app.module.ts
+++ TaskManagerWeb/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ViewTaskComponent } from './view-task/view-task.component';
+import { AddTaskComponent } from './add-task/add-task.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ViewTaskComponent,
+    AddTaskComponent
   ],
   imports: [
     BrowserModule,
