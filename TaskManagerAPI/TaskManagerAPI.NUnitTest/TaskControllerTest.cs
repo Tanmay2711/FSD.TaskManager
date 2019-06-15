9beae750bd6cb5f71080e3caf35762774dd2464e
Tanmay Vartak <Tanmay.Vartak@ey.com>
2019-06-15 14:02:07 +0530

    Added Test cases for Api methods

--- TaskManagerAPI/TaskManagerAPI.NUnitTest/TaskControllerTest.cs
+++ TaskManagerAPI/TaskManagerAPI.NUnitTest/TaskControllerTest.cs
@@ -17,46 +17,93 @@ namespace Tests
         private TaskManagerContext context;
         private TasksController controller;
 
+        public static IEnumerable<TestCaseData> TestCaseSourceForPutTask
+        {
+            get
+            {
+                yield return new TestCaseData(1, new Tasks
+                {
+                    Name = "Task 2",
+                    TasksID = 1,
+                    ParentID = 2,
+                    EndDate = DateTime.Now.AddDays(3),
+                    Priority = 4,
+                    StartDate = DateTime.Now
+                });
+                yield return new TestCaseData(2, new Tasks
+                {
+                    Name = "Task 1",
+                    TasksID = 3,
+                    ParentID = 0,
+                    EndDate = DateTime.Now.AddDays(6),
+                    Priority = 2,
+                    StartDate = DateTime.Now
+                });
+            }
+        }
+
+        public static IEnumerable<TestCaseData> TestCaseSourceForPostTask
+        {
+            get
+            {
+                yield return new TestCaseData(new Tasks
+                {
+                    Name = "New Task",
+                    TasksID = 0,
+                    ParentID = 2,
+                    EndDate = DateTime.Now.AddDays(6),
+                    Priority = 20,
+                    StartDate = DateTime.Now
+                });
+            }
+        }
+
         [OneTimeSetUp]
         public async Task SetupAsync()
         {
             var options = new DbContextOptionsBuilder<TaskManagerContext>()
-                       .UseInMemoryDatabase(databaseName: "InMemoryTaskManagerDatabase")
+                       .UseInMemoryDatabase(databaseName: "InMemoryTaskManagerDatabase").UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking)
                        .Options;
 
             this.context = new TaskManagerContext(options);
+
+            var t1 = new Tasks
+            {
+                Name = "Task 1",
+                TasksID = 1,
+                ParentID = 0,
+                EndDate = DateTime.Now.AddDays(7),
+                Priority = 1,
+                StartDate = DateTime.Now
+            };
+            var t2 = new Tasks
+            {
+                Name = "Task 2",
+                TasksID = 2,
+                ParentID = 1,
+                EndDate = DateTime.Now.AddDays(5),
+                Priority = 1,
+                StartDate = DateTime.Now
+            };
+            var t3 = new Tasks
+            {
+                Name = "Task 3",
+                TasksID = 3,
+                ParentID = 2,
+                EndDate = DateTime.Now,
+                Priority = 1,
+                StartDate = DateTime.Now.AddDays(-5)
+            };
             this.context.Tasks.AddRange(new Tasks[]
             {
-                new Tasks
-                {
-                    Name = "Task 1",
-                    TasksID = 1,
-                    ParentID = 0,
-                    EndDate = DateTime.Now.AddDays(7),
-                    Priority = 1,
-                    StartDate = DateTime.Now
-                },
-                new Tasks
-                {
-                    Name = "Task 2",
-                    TasksID = 2,
-                    ParentID = 1,
-                    EndDate = DateTime.Now.AddDays(5),
-                    Priority = 1,
-                    StartDate = DateTime.Now
-                },
-                new Tasks
-                {
-                    Name = "Task 3",
-                    TasksID = 3,
-                    ParentID = 2,
-                    EndDate = DateTime.Now,
-                    Priority = 1,
-                    StartDate = DateTime.Now.AddDays(-5)
-                }
+                t1,t2,t3
             });
 
             await this.context.SaveChangesAsync();
+
+            this.context.Entry<Tasks>(t1).State = EntityState.Detached;
+            this.context.Entry<Tasks>(t2).State = EntityState.Detached;
+            this.context.Entry<Tasks>(t3).State = EntityState.Detached;
 
             //setup controller object;
             this.controller = new TasksController(this.context);
@@ -70,6 +117,51 @@ namespace Tests
             Assert.IsInstanceOf<ActionResult<IEnumerable<Tasks>>>(res,"Return type must be ActionResult");
             Assert.IsNotNull(res.Value, "Action result value must not be null");
             Assert.AreEqual(lst.Count, res.Value.Count(), "Tasks count should match with the count from Tasks table");
+        }
+
+        [TestCase(1)]
+        [TestCase(2)]
+        [TestCase(3)]
+        [TestCase(4)]
+        public async Task TestTasksGetTasksByIdApi(int taskId)
+        {
+            var lst = (await context.Tasks.ToListAsync()).Find(t => t.TasksID.Equals(taskId));
+            var res = await this.controller.GetTasks(taskId);
+            if(taskId == 4)
+            {
+                Assert.IsInstanceOf<NotFoundResult>(res.Result, "in case of record not found it should return NotFoundResult");
+                return;
+            }
+            Assert.IsInstanceOf<ActionResult<Tasks>>(res, "Return type must be ActionResult");
+            Assert.IsNotNull(res.Value, "Action result value must not be null");
+            Assert.AreEqual(lst.TasksID, res.Value.TasksID, "Tasks count should match with the count from Tasks table");
+        }
+
+        [TestCaseSource("TestCaseSourceForPutTask")]
+        public async Task TestTasksPutTaskApi(int taskId, Tasks task)
+        {
+            var res = await this.controller.PutTasks(taskId, task);
+            var lst = (await context.Tasks.ToListAsync()).Find(t => t.TasksID.Equals(task.TasksID));
+            if (taskId != task.TasksID)
+            {
+                Assert.IsInstanceOf<BadRequestResult>(res, "in case of bad request it should return BadRequestResult");
+                return;
+            }
+
+            Assert.IsInstanceOf<NoContentResult>(res, "Return type must be ActionResult");
+            Assert.AreEqual(task.Name, lst.Name, "Tasks propertie should match with the request parameter after successful updates");
+        }
+
+        [TestCaseSource("TestCaseSourceForPostTask")]
+        public async Task TestTasksPostTaskApi(Tasks task)
+        {
+            var lst = (await context.Tasks.ToListAsync()).Last();
+            task.TasksID = lst.TasksID + 1;
+            var res = await this.controller.PostTasks(task);
+            lst = (await context.Tasks.ToListAsync()).Last();
+            Assert.IsInstanceOf<CreatedAtActionResult>(res.Result, "Return type must be CreatedAtActionResult");
+            Assert.AreEqual(task.Name, lst.Name, "Tasks propertie should match with the request parameter after successful post");
+            Assert.AreEqual((((ObjectResult)res.Result).Value as Tasks).TasksID, task.TasksID, "TaskId should match after sucessfull post");
         }
     }
 }
