72391d004594462bf3924ba4697e3e9cd3ebe887
Tanmay Vartak <Tanmay.Vartak@ey.com>
2019-06-15 10:25:24 +0530

    Added changes for nunit test cases

--- TaskManagerAPI/TaskManagerAPI.NUnitTest/TaskControllerTest.cs
+++ TaskManagerAPI/TaskManagerAPI.NUnitTest/TaskControllerTest.cs
@@ -1,18 +1,69 @@
+using Microsoft.EntityFrameworkCore;
 using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using TaskManagerAPI.Controllers;
+using TaskManagerAPI.Models;
 
 namespace Tests
 {
     [TestFixture]
     public class TaskControllerTest
     {
-        [SetUp]
-        public void Setup()
+        private TaskManagerContext context;
+        private TasksController controller;
+
+        [OneTimeSetUp]
+        public async Task SetupAsync()
         {
+            var options = new DbContextOptionsBuilder<TaskManagerContext>()
+                       .UseInMemoryDatabase(databaseName: "InMemoryTaskManagerDatabase")
+                       .Options;
+
+            this.context = new TaskManagerContext(options);
+            this.context.Tasks.AddRange(new Tasks[]
+            {
+                new Tasks
+                {
+                    Name = "Task 1",
+                    TasksID = 1,
+                    ParentID = 0,
+                    EndDate = DateTime.Now.AddDays(7),
+                    Priority = 1,
+                    StartDate = DateTime.Now
+                },
+                new Tasks
+                {
+                    Name = "Task 2",
+                    TasksID = 2,
+                    ParentID = 1,
+                    EndDate = DateTime.Now.AddDays(5),
+                    Priority = 1,
+                    StartDate = DateTime.Now
+                },
+                new Tasks
+                {
+                    Name = "Task 3",
+                    TasksID = 3,
+                    ParentID = 2,
+                    EndDate = DateTime.Now,
+                    Priority = 1,
+                    StartDate = DateTime.Now.AddDays(-5)
+                }
+            });
+
+            await this.context.SaveChangesAsync();
+
+            //setup controller object;
+            this.controller = new TasksController(this.context);
         }
 
         [Test]
-        public void Test1()
+        public async Task Test1Async()
         {
+            var lst = await context.Tasks.ToListAsync();
             Assert.Pass();
         }
     }
